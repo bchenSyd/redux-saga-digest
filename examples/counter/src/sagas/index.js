48e62b14d371f93503a8c26cf826607b687fea36
Bo Chen <bochen2014@yahoo.com>
2018-05-23 00:53:10 +1000

    update

--- examples/counter/src/sagas/index.js
+++ examples/counter/src/sagas/index.js
@@ -19,27 +19,15 @@ function* test() {
 
 
 export function* incrementAsync() {
-
-  // > https://github.com/redux-saga/redux-saga/issues/1448#issuecomment-390896294
-  // yield test(); yieldIterator better be replaced with `yield call(Generator)`
-  yield call(test); // use the Call Effect form
-  // or yield fork(test); // the newly forked task will become current task (root task) 's subtask, so that it won't be orphaned after the end of current workflow is reached;
-
-  console.log('calling 1 is done')
-
-
-  yield* test();
-  console.log('calling 2 is done')
+  yield put({type:'SECOND'})
+  console.log('worker saga is done')
 }
 
 export default function* rootSaga() {
-  yield takeEvery('INCREMENT_ASYNC', function* () {
-    yield all([
-      takeEvery('SECOND', function* () {
-        yield put({ type: 'THIRD' });
-      }),
-      put({ type: 'SECOND' })
-    ])
-    console.log('done')
-  })
+  /* takeEvery = Fork(()=>{
+          yield take('INCREMENT_ASYNC ');
+          yield* worker();
+  })*/
+  yield fork(incrementAsync)
+  console.log('root sage done;')
 }
