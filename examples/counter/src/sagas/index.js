0b599cae605673802d721c73cf96d6def7a32244
Bo Chen <bo.chen@westpac.com.au>
2018-05-23 09:47:35 +1000

    update

--- examples/counter/src/sagas/index.js
+++ examples/counter/src/sagas/index.js
@@ -3,31 +3,15 @@
 import { put, call, fork, all,  takeEvery, delay } from 'redux-saga/effects'
 import { debug } from 'util';
 
-function* test() {
-  yield new Promise(resolve => {
-    setTimeout(() => {
-      resolve(true);
-    }, 2000);
-  });
-
-  yield delay(1000);
-
-  yield call(() => {
-    console.log("I'm forked");
-  })
-}
-
-
-export function* incrementAsync() {
-  yield put({type:'SECOND'})
-  console.log('worker saga is done')
-}
 
 export default function* rootSaga() {
-  /* takeEvery = Fork(()=>{
-          yield take('INCREMENT_ASYNC ');
-          yield* worker();
-  })*/
-  yield fork(incrementAsync)
-  console.log('root sage done;')
+  yield takeEvery('INCREMENT_ASYNC', function* () {
+    yield all([
+      takeEvery('SECOND', function* () {
+        yield put({ type: 'THIRD' });
+      }),
+      put({ type: 'SECOND' })
+    ])
+    console.log('done')
+  })
 }
