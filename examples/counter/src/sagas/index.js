b9fa8aa0b04ce41d0e6cf601e1cd554651c1f55a
Bo Chen <bo.chen@westpac.com.au>
2018-05-23 11:39:24 +1000

    demistify

--- examples/counter/src/sagas/index.js
+++ examples/counter/src/sagas/index.js
@@ -6,8 +6,10 @@ import { debug } from 'util';
 
 export default function* rootSaga() {
   yield takeEvery('INCREMENT_ASYNC', function* () {
-    yield takeEvery('SECOND', function* () {
-          console.log('******************  SECOND CAPTURED')
+    // DANGER: takeEvery subscribe to redux store and will NEVER finish;
+    //         if you click the button twice you will now have 2 subscribers to your redux store;
+    yield takeEvery('SECOND', function* () { 
+          console.log('******************  SECOND CAPTURED') // therefore you will see this message twice when you click the button for the second time;
     });
     yield put({ type: 'SECOND' })
   })
